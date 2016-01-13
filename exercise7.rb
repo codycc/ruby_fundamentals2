6a2bafb5d52172dcbb1f85494464b2ab8afe72f7
Cody Condon <codycondon50@gmail.com>
2016-01-13 16:08:28 -0500

    Finished exercise 7

--- exercise7.rb
+++ exercise7.rb
@@ -4,12 +4,44 @@ students = {
   :cohort3 => 22
 }
 
-def display
-students.each do |key,value|
-  puts "#{key}, #{value} students"
 
+#creating the method
+def displaystudents(studenthash)
+ studenthash.each {|cohort,num|
+   puts " #{cohort} :#{num} students"
+ }
 end
+#displaying students
+displaystudents(students)
 
-end
+#adding cohort4 object to hash
 
-display
+students[:cohort4] = 43
+
+#outputting the keys
+puts students.keys
+
+#Expanding classroom
+students.each {|cohort,number|
+  students[cohort] = number * 1.05
+}
+
+displaystudents(students)
+
+#Deleting the second cohort
+
+students.delete(:cohort2)
+
+#displaying students again
+displaystudents(students)
+
+#BONUS
+
+totalofstudents = 0
+
+students.each { |cohort,number|
+  totalofstudents += number
+
+}
+
+puts "The total number of students is  #{totalofstudents}"
